1679b8a41a8f32de0588b59715cd2544d54d4b7e
opyale <example@example.com>
2020-04-07 23:37:46 +0200

    Reformatting code.

--- app/src/main/java/org/mian/gitnex/activities/BaseActivity.java
+++ app/src/main/java/org/mian/gitnex/activities/BaseActivity.java
@@ -12,61 +12,61 @@ import org.mian.gitnex.util.TinyDB;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
 
-        final TinyDB tinyDb = new TinyDB(getApplicationContext());
+		final TinyDB tinyDb = new TinyDB(getApplicationContext());
 
-        if(tinyDb.getInt("themeId") == 1) {
-            setTheme(R.style.AppThemeLight);
-        }
-        else {
-            setTheme(R.style.AppTheme);
-        }
+		if(tinyDb.getInt("themeId") == 1) {
+			setTheme(R.style.AppThemeLight);
+		}
+		else {
+			setTheme(R.style.AppTheme);
+		}
 
-        super.onCreate(savedInstanceState);
-        setContentView(getLayoutResourceId());
+		super.onCreate(savedInstanceState);
+		setContentView(getLayoutResourceId());
 
-        if(tinyDb.getInt("customFontId") == 0) {
+		if(tinyDb.getInt("customFontId") == 0) {
 
-            FontsOverride.setDefaultFont(this, "DEFAULT", "fonts/roboto.ttf");
-            FontsOverride.setDefaultFont(this, "MONOSPACE", "fonts/roboto.ttf");
-            FontsOverride.setDefaultFont(this, "SERIF", "fonts/roboto.ttf");
-            FontsOverride.setDefaultFont(this, "SANS_SERIF", "fonts/roboto.ttf");
+			FontsOverride.setDefaultFont(this, "DEFAULT", "fonts/roboto.ttf");
+			FontsOverride.setDefaultFont(this, "MONOSPACE", "fonts/roboto.ttf");
+			FontsOverride.setDefaultFont(this, "SERIF", "fonts/roboto.ttf");
+			FontsOverride.setDefaultFont(this, "SANS_SERIF", "fonts/roboto.ttf");
 
-        }
-        else if (tinyDb.getInt("customFontId") == 1) {
+		}
+		else if(tinyDb.getInt("customFontId") == 1) {
 
-            FontsOverride.setDefaultFont(this, "DEFAULT", "fonts/manroperegular.ttf");
-            FontsOverride.setDefaultFont(this, "MONOSPACE", "fonts/manroperegular.ttf");
-            FontsOverride.setDefaultFont(this, "SERIF", "fonts/manroperegular.ttf");
-            FontsOverride.setDefaultFont(this, "SANS_SERIF", "fonts/manroperegular.ttf");
+			FontsOverride.setDefaultFont(this, "DEFAULT", "fonts/manroperegular.ttf");
+			FontsOverride.setDefaultFont(this, "MONOSPACE", "fonts/manroperegular.ttf");
+			FontsOverride.setDefaultFont(this, "SERIF", "fonts/manroperegular.ttf");
+			FontsOverride.setDefaultFont(this, "SANS_SERIF", "fonts/manroperegular.ttf");
 
-        }
-        else if (tinyDb.getInt("customFontId") == 2) {
+		}
+		else if(tinyDb.getInt("customFontId") == 2) {
 
-            FontsOverride.setDefaultFont(this, "DEFAULT", "fonts/sourcecodeproregular.ttf");
-            FontsOverride.setDefaultFont(this, "MONOSPACE", "fonts/sourcecodeproregular.ttf");
-            FontsOverride.setDefaultFont(this, "SERIF", "fonts/sourcecodeproregular.ttf");
-            FontsOverride.setDefaultFont(this, "SANS_SERIF", "fonts/sourcecodeproregular.ttf");
+			FontsOverride.setDefaultFont(this, "DEFAULT", "fonts/sourcecodeproregular.ttf");
+			FontsOverride.setDefaultFont(this, "MONOSPACE", "fonts/sourcecodeproregular.ttf");
+			FontsOverride.setDefaultFont(this, "SERIF", "fonts/sourcecodeproregular.ttf");
+			FontsOverride.setDefaultFont(this, "SANS_SERIF", "fonts/sourcecodeproregular.ttf");
 
-        }
-        else {
+		}
+		else {
 
-            FontsOverride.setDefaultFont(this, "DEFAULT", "fonts/roboto.ttf");
-            FontsOverride.setDefaultFont(this, "MONOSPACE", "fonts/roboto.ttf");
-            FontsOverride.setDefaultFont(this, "SERIF", "fonts/roboto.ttf");
-            FontsOverride.setDefaultFont(this, "SANS_SERIF", "fonts/roboto.ttf");
+			FontsOverride.setDefaultFont(this, "DEFAULT", "fonts/roboto.ttf");
+			FontsOverride.setDefaultFont(this, "MONOSPACE", "fonts/roboto.ttf");
+			FontsOverride.setDefaultFont(this, "SERIF", "fonts/roboto.ttf");
+			FontsOverride.setDefaultFont(this, "SANS_SERIF", "fonts/roboto.ttf");
 
-        }
+		}
 
-        // enabling counter badges by default
-        if(!tinyDb.getBoolean("enableCounterBadgesInit")) {
-            tinyDb.putBoolean("enableCounterBadges", true);
-            tinyDb.putBoolean("enableCounterBadgesInit", true);
-        }
+		// enabling counter badges by default
+		if(!tinyDb.getBoolean("enableCounterBadgesInit")) {
+			tinyDb.putBoolean("enableCounterBadges", true);
+			tinyDb.putBoolean("enableCounterBadgesInit", true);
+		}
 
-        // setting default font
+		// setting default font
 		if(!tinyDb.getBoolean("customFontStrInit")) {
 			tinyDb.putString("customFontStr", "Manrope");
 			tinyDb.putInt("customFontId", 1);
@@ -74,9 +74,9 @@ public abstract class BaseActivity extends AppCompatActivity {
 			tinyDb.putBoolean("customFontStrInit", true);
 		}
 
-    }
+	}
 
-    protected abstract int getLayoutResourceId();
+	protected abstract int getLayoutResourceId();
 
 }
 
