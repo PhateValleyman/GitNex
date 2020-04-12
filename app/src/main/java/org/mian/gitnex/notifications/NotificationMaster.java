9bb0d24c0414074492bb818c9e3795c93cb37663
opyale <example@example.com>
2020-04-12 12:56:26 +0200

    First version checking things.

--- app/src/main/java/org/mian/gitnex/notifications/NotificationMaster.java
+++ app/src/main/java/org/mian/gitnex/notifications/NotificationMaster.java
@@ -1,21 +1,48 @@
 package org.mian.gitnex.notifications;
 
 import android.content.Context;
+import androidx.annotation.NonNull;
 import androidx.work.Constraints;
 import androidx.work.NetworkType;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkManager;
+import org.mian.gitnex.clients.RetrofitClient;
+import org.mian.gitnex.models.GiteaVersion;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Author opyale
  */
 
 public class NotificationMaster {
+	private String notificationsSupported;
+
 	public NotificationMaster() {
 
 	}
 
-	public static void hireWorker(Context context) {
+	private void checkVersion(Context context) {
+
+		Call<GiteaVersion> versionCall = RetrofitClient.getInstance("", context).getApiInterface().getGiteaVersion();
+
+		versionCall.enqueue(new Callback<GiteaVersion>() {
+
+			@Override
+			public void onResponse(@NonNull Call<GiteaVersion> call, @NonNull Response<GiteaVersion> response) {
+
+			}
+
+			@Override
+			public void onFailure(@NonNull Call<GiteaVersion> call,@NonNull Throwable t) {
+
+			}
+		});
+
+	}
+
+	public void hireWorker(Context context) {
 
 		WorkManager.getInstance(context).cancelAllWorkByTag("gitnex-notifications");
 
@@ -35,4 +62,5 @@ public class NotificationMaster {
 		WorkManager.getInstance(context).enqueue(notificationRequest);
 
 	}
+
 }
