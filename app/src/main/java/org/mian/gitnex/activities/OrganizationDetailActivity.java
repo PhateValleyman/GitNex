b283b01b919e625eb87c0e988a00cabda63f4b60
6543 <6543@obermui.de>
2020-05-23 18:17:51 +0200

    ReFormat OrganizationDetailActivity.java

--- app/src/main/java/org/mian/gitnex/activities/OrganizationDetailActivity.java
+++ app/src/main/java/org/mian/gitnex/activities/OrganizationDetailActivity.java
@@ -1,12 +1,5 @@
 package org.mian.gitnex.activities;
 
-import com.google.android.material.tabs.TabLayout;
-import androidx.annotation.NonNull;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
@@ -17,9 +10,16 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import com.google.android.material.tabs.TabLayout;
 import org.mian.gitnex.R;
-import org.mian.gitnex.fragments.MembersByOrgFragment;
 import org.mian.gitnex.fragments.BottomSheetOrganizationFragment;
+import org.mian.gitnex.fragments.MembersByOrgFragment;
 import org.mian.gitnex.fragments.OrganizationInfoFragment;
 import org.mian.gitnex.fragments.RepositoriesByOrgFragment;
 import org.mian.gitnex.fragments.TeamsByOrgFragment;
@@ -32,158 +32,164 @@ import java.util.Objects;
 
 public class OrganizationDetailActivity extends BaseActivity implements BottomSheetOrganizationFragment.BottomSheetListener {
 
-    final Context ctx = this;
-    private Context appCtx;
+	final Context ctx = this;
+	private Context appCtx;
 
-    @Override
-    protected int getLayoutResourceId(){
-        return R.layout.activity_org_detail;
-    }
+	@Override
+	protected int getLayoutResourceId() {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+		return R.layout.activity_org_detail;
+	}
 
-        super.onCreate(savedInstanceState);
-        appCtx = getApplicationContext();
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
 
-        TinyDB tinyDb = new TinyDB(appCtx);
-        String orgName = tinyDb.getString("orgName");
+		super.onCreate(savedInstanceState);
+		appCtx = getApplicationContext();
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        TextView toolbarTitle = toolbar.findViewById(R.id.toolbar_title);
+		TinyDB tinyDb = new TinyDB(appCtx);
+		String orgName = tinyDb.getString("orgName");
 
-        setSupportActionBar(toolbar);
-        Objects.requireNonNull(getSupportActionBar()).setTitle(orgName);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		TextView toolbarTitle = toolbar.findViewById(R.id.toolbar_title);
 
-        OrganizationDetailActivity.SectionsPagerAdapter mSectionsPagerAdapter = new OrganizationDetailActivity.SectionsPagerAdapter(getSupportFragmentManager());
+		setSupportActionBar(toolbar);
+		Objects.requireNonNull(getSupportActionBar()).setTitle(orgName);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        ViewPager mViewPager = findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+		OrganizationDetailActivity.SectionsPagerAdapter mSectionsPagerAdapter = new OrganizationDetailActivity.SectionsPagerAdapter(getSupportFragmentManager());
 
-        TabLayout tabLayout = findViewById(R.id.tabs);
+		ViewPager mViewPager = findViewById(R.id.container);
+		mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        Typeface myTypeface;
+		TabLayout tabLayout = findViewById(R.id.tabs);
 
-        switch(tinyDb.getInt("customFontId", -1)) {
+		Typeface myTypeface;
 
-            case 0:
-                myTypeface = Typeface.createFromAsset(ctx.getAssets(), "fonts/roboto.ttf");
-                break;
+		switch(tinyDb.getInt("customFontId", -1)) {
 
-            case 2:
-                myTypeface = Typeface.createFromAsset(ctx.getAssets(), "fonts/sourcecodeproregular.ttf");
-                break;
+			case 0:
+				myTypeface = Typeface.createFromAsset(ctx.getAssets(), "fonts/roboto.ttf");
+				break;
 
-            default:
-                myTypeface = Typeface.createFromAsset(ctx.getAssets(), "fonts/manroperegular.ttf");
-                break;
+			case 2:
+				myTypeface = Typeface.createFromAsset(ctx.getAssets(), "fonts/sourcecodeproregular.ttf");
+				break;
 
-        }
+			default:
+				myTypeface = Typeface.createFromAsset(ctx.getAssets(), "fonts/manroperegular.ttf");
+				break;
 
-        toolbarTitle.setTypeface(myTypeface);
-        toolbarTitle.setText(orgName);
+		}
 
-        ViewGroup vg = (ViewGroup) tabLayout.getChildAt(0);
-        int tabsCount = vg.getChildCount();
-        for (int j = 0; j < tabsCount; j++) {
-            ViewGroup vgTab = (ViewGroup) vg.getChildAt(j);
-            int tabChildCount = vgTab.getChildCount();
-            for (int i = 0; i < tabChildCount; i++) {
-                View tabViewChild = vgTab.getChildAt(i);
-                if (tabViewChild instanceof TextView) {
-                    ((TextView) tabViewChild).setTypeface(myTypeface);
-                }
-            }
-        }
+		toolbarTitle.setTypeface(myTypeface);
+		toolbarTitle.setText(orgName);
 
-        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+		ViewGroup vg = (ViewGroup) tabLayout.getChildAt(0);
+		int tabsCount = vg.getChildCount();
+		for(int j = 0; j < tabsCount; j++) {
+			ViewGroup vgTab = (ViewGroup) vg.getChildAt(j);
+			int tabChildCount = vgTab.getChildCount();
+			for(int i = 0; i < tabChildCount; i++) {
+				View tabViewChild = vgTab.getChildAt(i);
+				if(tabViewChild instanceof TextView) {
+					((TextView) tabViewChild).setTypeface(myTypeface);
+				}
+			}
+		}
 
-    }
+		mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+		tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.repo_dotted_menu, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.repo_dotted_menu, menu);
+		return true;
+	}
 
-        int id = item.getItemId();
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch (id) {
-            case android.R.id.home:
-                finish();
-                return true;
-            case R.id.repoMenu:
-                BottomSheetOrganizationFragment bottomSheet = new BottomSheetOrganizationFragment();
-                bottomSheet.show(getSupportFragmentManager(), "orgBottomSheet");
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+		int id = item.getItemId();
 
-    }
+		switch(id) {
+			case android.R.id.home:
+				finish();
+				return true;
+			case R.id.repoMenu:
+				BottomSheetOrganizationFragment bottomSheet = new BottomSheetOrganizationFragment();
+				bottomSheet.show(getSupportFragmentManager(), "orgBottomSheet");
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
 
-    @Override
-    public void onButtonClicked(String text) {
+	}
 
-        TinyDB tinyDb = new TinyDB(appCtx);
+	@Override
+	public void onButtonClicked(String text) {
 
-        switch (text) {
-            case "repository":
-                tinyDb.putBoolean("organizationAction", true);
-                startActivity(new Intent(OrganizationDetailActivity.this, CreateRepoActivity.class));
-                break;
-            case "team":
-                startActivity(new Intent(OrganizationDetailActivity.this, CreateTeamByOrgActivity.class));
-                break;
-        }
-        //Log.i("clicked", text);
+		TinyDB tinyDb = new TinyDB(appCtx);
 
-    }
+		switch(text) {
+			case "repository":
+				tinyDb.putBoolean("organizationAction", true);
+				startActivity(new Intent(OrganizationDetailActivity.this, CreateRepoActivity.class));
+				break;
+			case "team":
+				startActivity(new Intent(OrganizationDetailActivity.this, CreateTeamByOrgActivity.class));
+				break;
+		}
+		//Log.i("clicked", text);
 
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+	}
 
-        SectionsPagerAdapter(FragmentManager fm) {
-            super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        }
+	public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-        @NonNull
-        @Override
-        public Fragment getItem(int position) {
+		SectionsPagerAdapter(FragmentManager fm) {
 
-            TinyDB tinyDb = new TinyDB(appCtx);
-            String orgName;
-            if(getIntent().getStringExtra("orgName") != null || !Objects.equals(getIntent().getStringExtra("orgName"), "")) {
-                orgName = getIntent().getStringExtra("orgName");
-            }
-            else {
-                orgName = tinyDb.getString("orgName");
-            }
+			super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+		}
 
-            Fragment fragment = null;
-            switch (position) {
-                case 0: // info
-                    return OrganizationInfoFragment.newInstance(orgName);
-                case 1: // repos
-                    return RepositoriesByOrgFragment.newInstance(orgName);
-                case 2: // teams
-                    return TeamsByOrgFragment.newInstance(orgName);
-                case 3: // members
-                    return MembersByOrgFragment.newInstance(orgName);
-            }
-            return fragment;
+		@NonNull
+		@Override
+		public Fragment getItem(int position) {
 
-        }
+			TinyDB tinyDb = new TinyDB(appCtx);
+			String orgName;
+			if(getIntent().getStringExtra("orgName") != null || !Objects.equals(getIntent().getStringExtra("orgName"), "")) {
+				orgName = getIntent().getStringExtra("orgName");
+			}
+			else {
+				orgName = tinyDb.getString("orgName");
+			}
 
-        @Override
-        public int getCount() {
-            return 4;
-        }
-    }
+			Fragment fragment = null;
+			switch(position) {
+				case 0: // info
+					return OrganizationInfoFragment.newInstance(orgName);
+				case 1: // repos
+					return RepositoriesByOrgFragment.newInstance(orgName);
+				case 2: // teams
+					return TeamsByOrgFragment.newInstance(orgName);
+				case 3: // members
+					return MembersByOrgFragment.newInstance(orgName);
+			}
+			return fragment;
+
+		}
+
+		@Override
+		public int getCount() {
+
+			return 4;
+		}
+
+	}
+
 }
