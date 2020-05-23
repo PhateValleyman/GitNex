558adab537fde0bc4d76bfd79429b889cc57fc0c
6543 <6543@obermui.de>
2020-05-23 18:17:40 +0200

    ReFormat CreateMilestoneActivity.java

--- app/src/main/java/org/mian/gitnex/activities/CreateMilestoneActivity.java
+++ app/src/main/java/org/mian/gitnex/activities/CreateMilestoneActivity.java
@@ -1,8 +1,5 @@
 package org.mian.gitnex.activities;
 
-import androidx.annotation.NonNull;
-import retrofit2.Call;
-import retrofit2.Callback;
 import android.app.DatePickerDialog;
 import android.content.Context;
 import android.graphics.drawable.GradientDrawable;
@@ -14,16 +11,19 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import androidx.annotation.NonNull;
 import org.mian.gitnex.R;
 import org.mian.gitnex.clients.RetrofitClient;
 import org.mian.gitnex.helpers.AlertDialogs;
 import org.mian.gitnex.helpers.Authorization;
-import org.mian.gitnex.helpers.Version;
 import org.mian.gitnex.helpers.Toasty;
+import org.mian.gitnex.helpers.Version;
 import org.mian.gitnex.models.Milestones;
 import org.mian.gitnex.util.AppUtil;
 import org.mian.gitnex.util.TinyDB;
 import java.util.Calendar;
+import retrofit2.Call;
+import retrofit2.Callback;
 
 /**
  * Author M M Arif
@@ -31,227 +31,228 @@ import java.util.Calendar;
 
 public class CreateMilestoneActivity extends BaseActivity implements View.OnClickListener {
 
-    private EditText milestoneDueDate;
-    private View.OnClickListener onClickListener;
-    private EditText milestoneTitle;
-    private EditText milestoneDescription;
-    private Button createNewMilestoneButton;
-    final Context ctx = this;
-    private Context appCtx;
+	private EditText milestoneDueDate;
+	private View.OnClickListener onClickListener;
+	private EditText milestoneTitle;
+	private EditText milestoneDescription;
+	private Button createNewMilestoneButton;
+	final Context ctx = this;
+	private Context appCtx;
 
-    @Override
-    protected int getLayoutResourceId(){
-        return R.layout.activity_new_milestone;
-    }
+	@Override
+	protected int getLayoutResourceId() {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+		return R.layout.activity_new_milestone;
+	}
 
-        super.onCreate(savedInstanceState);
-        appCtx = getApplicationContext();
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
 
-        boolean connToInternet = AppUtil.haveNetworkConnection(appCtx);
+		super.onCreate(savedInstanceState);
+		appCtx = getApplicationContext();
 
-        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+		boolean connToInternet = AppUtil.haveNetworkConnection(appCtx);
 
-        milestoneDueDate = findViewById(R.id.milestoneDueDate);
-        ImageView closeActivity = findViewById(R.id.close);
-        createNewMilestoneButton = findViewById(R.id.createNewMilestoneButton);
-        milestoneTitle = findViewById(R.id.milestoneTitle);
-        milestoneDescription = findViewById(R.id.milestoneDescription);
+		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 
-        milestoneTitle.requestFocus();
-        assert imm != null;
-        imm.showSoftInput(milestoneTitle, InputMethodManager.SHOW_IMPLICIT);
+		milestoneDueDate = findViewById(R.id.milestoneDueDate);
+		ImageView closeActivity = findViewById(R.id.close);
+		createNewMilestoneButton = findViewById(R.id.createNewMilestoneButton);
+		milestoneTitle = findViewById(R.id.milestoneTitle);
+		milestoneDescription = findViewById(R.id.milestoneDescription);
 
-        initCloseListener();
-        closeActivity.setOnClickListener(onClickListener);
-        milestoneDueDate.setOnClickListener(this);
+		milestoneTitle.requestFocus();
+		assert imm != null;
+		imm.showSoftInput(milestoneTitle, InputMethodManager.SHOW_IMPLICIT);
 
-        if(!connToInternet) {
+		initCloseListener();
+		closeActivity.setOnClickListener(onClickListener);
+		milestoneDueDate.setOnClickListener(this);
 
-            createNewMilestoneButton.setEnabled(false);
-            GradientDrawable shape =  new GradientDrawable();
-            shape.setCornerRadius( 8 );
-            shape.setColor(getResources().getColor(R.color.hintColor));
-            createNewMilestoneButton.setBackground(shape);
+		if(!connToInternet) {
 
-        } else {
+			createNewMilestoneButton.setEnabled(false);
+			GradientDrawable shape = new GradientDrawable();
+			shape.setCornerRadius(8);
+			shape.setColor(getResources().getColor(R.color.hintColor));
+			createNewMilestoneButton.setBackground(shape);
 
-            createNewMilestoneButton.setOnClickListener(createMilestoneListener);
+		}
+		else {
 
-        }
+			createNewMilestoneButton.setOnClickListener(createMilestoneListener);
 
-    }
+		}
 
-    private View.OnClickListener createMilestoneListener = new View.OnClickListener() {
-        public void onClick(View v) {
-            processNewMilestone();
-        }
-    };
+	}
 
-    private void processNewMilestone() {
+	private View.OnClickListener createMilestoneListener = new View.OnClickListener() {
 
-        boolean connToInternet = AppUtil.haveNetworkConnection(appCtx);
-        AppUtil appUtil = new AppUtil();
-        TinyDB tinyDb = new TinyDB(appCtx);
-        String repoFullName = tinyDb.getString("repoFullName");
-        String[] parts = repoFullName.split("/");
-        final String repoOwner = parts[0];
-        final String repoName = parts[1];
-        final String instanceUrl = tinyDb.getString("instanceUrl");
-        final String loginUid = tinyDb.getString("loginUid");
-        final String instanceToken = "token " + tinyDb.getString(loginUid + "-token");
-        //String appLocale = tinyDb.getString("locale");
+		public void onClick(View v) {
 
-        String newMilestoneTitle = milestoneTitle.getText().toString();
-        String newMilestoneDescription = milestoneDescription.getText().toString();
-        String newMilestoneDueDate = milestoneDueDate.getText().toString();
+			processNewMilestone();
+		}
+	};
 
-        if(!connToInternet) {
+	private void processNewMilestone() {
 
-            Toasty.info(ctx, getResources().getString(R.string.checkNetConnection));
-            return;
+		boolean connToInternet = AppUtil.haveNetworkConnection(appCtx);
+		AppUtil appUtil = new AppUtil();
+		TinyDB tinyDb = new TinyDB(appCtx);
+		String repoFullName = tinyDb.getString("repoFullName");
+		String[] parts = repoFullName.split("/");
+		final String repoOwner = parts[0];
+		final String repoName = parts[1];
+		final String instanceUrl = tinyDb.getString("instanceUrl");
+		final String loginUid = tinyDb.getString("loginUid");
+		final String instanceToken = "token " + tinyDb.getString(loginUid + "-token");
+		//String appLocale = tinyDb.getString("locale");
 
-        }
+		String newMilestoneTitle = milestoneTitle.getText().toString();
+		String newMilestoneDescription = milestoneDescription.getText().toString();
+		String newMilestoneDueDate = milestoneDueDate.getText().toString();
 
-        if(newMilestoneTitle.equals("")) {
+		if(!connToInternet) {
 
-            Toasty.info(ctx, getString(R.string.milestoneNameErrorEmpty));
-            return;
+			Toasty.info(ctx, getResources().getString(R.string.checkNetConnection));
+			return;
 
-        }
+		}
 
-        if(!newMilestoneDescription.equals("")) {
-            if (appUtil.charactersLength(newMilestoneDescription) > 255) {
+		if(newMilestoneTitle.equals("")) {
 
-                Toasty.info(ctx, getString(R.string.milestoneDescError));
-                return;
+			Toasty.info(ctx, getString(R.string.milestoneNameErrorEmpty));
+			return;
 
-            }
-        }
+		}
 
-        String finalMilestoneDueDate = null;
-        if(!newMilestoneDueDate.isEmpty()) {
-            finalMilestoneDueDate = (AppUtil.customDateCombine(AppUtil.customDateFormat(newMilestoneDueDate)));
-        } else if (new Version(tinyDb.getString("giteaVersion")).less("1.10.0")) {
-            // if Gitea version is less than 1.10.0 DueDate is required
-            Toasty.info(ctx, getString(R.string.milestoneDateEmpty));
-            return;
-        }
+		if(!newMilestoneDescription.equals("")) {
+			if(appUtil.charactersLength(newMilestoneDescription) > 255) {
 
-        disableProcessButton();
-        createNewMilestone(instanceUrl, Authorization.returnAuthentication(ctx, loginUid, instanceToken), repoOwner, repoName, newMilestoneTitle, newMilestoneDescription, finalMilestoneDueDate);
+				Toasty.info(ctx, getString(R.string.milestoneDescError));
+				return;
 
-    }
+			}
+		}
 
-    private void createNewMilestone(final String instanceUrl, final String token, String repoOwner, String repoName, String newMilestoneTitle, String newMilestoneDescription, String newMilestoneDueDate) {
+		String finalMilestoneDueDate = null;
+		if(!newMilestoneDueDate.isEmpty()) {
+			finalMilestoneDueDate = (AppUtil.customDateCombine(AppUtil.customDateFormat(newMilestoneDueDate)));
+		}
+		else if(new Version(tinyDb.getString("giteaVersion")).less("1.10.0")) {
+			// if Gitea version is less than 1.10.0 DueDate is required
+			Toasty.info(ctx, getString(R.string.milestoneDateEmpty));
+			return;
+		}
 
-        Milestones createMilestone = new Milestones(newMilestoneDescription, newMilestoneTitle, newMilestoneDueDate);
+		disableProcessButton();
+		createNewMilestone(instanceUrl, Authorization.returnAuthentication(ctx, loginUid, instanceToken), repoOwner, repoName, newMilestoneTitle, newMilestoneDescription, finalMilestoneDueDate);
 
-        Call<Milestones> call;
+	}
 
-        call = RetrofitClient
-                .getInstance(instanceUrl, ctx)
-                .getApiInterface()
-                .createMilestone(token, repoOwner, repoName, createMilestone);
+	private void createNewMilestone(final String instanceUrl, final String token, String repoOwner, String repoName, String newMilestoneTitle, String newMilestoneDescription, String newMilestoneDueDate) {
 
-        call.enqueue(new Callback<Milestones>() {
+		Milestones createMilestone = new Milestones(newMilestoneDescription, newMilestoneTitle, newMilestoneDueDate);
 
-            @Override
-            public void onResponse(@NonNull Call<Milestones> call, @NonNull retrofit2.Response<Milestones> response) {
+		Call<Milestones> call;
 
-                if(response.isSuccessful()) {
-                    if(response.code() == 201) {
+		call = RetrofitClient.getInstance(instanceUrl, ctx).getApiInterface().createMilestone(token, repoOwner, repoName, createMilestone);
 
-                        TinyDB tinyDb = new TinyDB(appCtx);
-                        tinyDb.putBoolean("milestoneCreated", true);
-                        Toasty.info(ctx, getString(R.string.milestoneCreated));
-                        enableProcessButton();
-                        finish();
+		call.enqueue(new Callback<Milestones>() {
 
-                    }
-                }
-                else if(response.code() == 401) {
+			@Override
+			public void onResponse(@NonNull Call<Milestones> call, @NonNull retrofit2.Response<Milestones> response) {
 
-                    enableProcessButton();
-                    AlertDialogs.authorizationTokenRevokedDialog(ctx, getResources().getString(R.string.alertDialogTokenRevokedTitle),
-                            getResources().getString(R.string.alertDialogTokenRevokedMessage),
-                            getResources().getString(R.string.alertDialogTokenRevokedCopyNegativeButton),
-                            getResources().getString(R.string.alertDialogTokenRevokedCopyPositiveButton));
+				if(response.isSuccessful()) {
+					if(response.code() == 201) {
 
-                }
-                else {
+						TinyDB tinyDb = new TinyDB(appCtx);
+						tinyDb.putBoolean("milestoneCreated", true);
+						Toasty.info(ctx, getString(R.string.milestoneCreated));
+						enableProcessButton();
+						finish();
 
-                    enableProcessButton();
-                    Toasty.info(ctx, getString(R.string.milestoneCreatedError));
+					}
+				}
+				else if(response.code() == 401) {
 
-                }
+					enableProcessButton();
+					AlertDialogs.authorizationTokenRevokedDialog(ctx, getResources().getString(R.string.alertDialogTokenRevokedTitle), getResources().getString(R.string.alertDialogTokenRevokedMessage), getResources().getString(R.string.alertDialogTokenRevokedCopyNegativeButton), getResources().getString(R.string.alertDialogTokenRevokedCopyPositiveButton));
 
-            }
+				}
+				else {
 
-            @Override
-            public void onFailure(@NonNull Call<Milestones> call, @NonNull Throwable t) {
-                Log.e("onFailure", t.toString());
-                enableProcessButton();
-            }
-        });
+					enableProcessButton();
+					Toasty.info(ctx, getString(R.string.milestoneCreatedError));
 
-    }
+				}
 
-    @Override
-    public void onClick(View v) {
+			}
 
-        if (v == milestoneDueDate) {
+			@Override
+			public void onFailure(@NonNull Call<Milestones> call, @NonNull Throwable t) {
 
-            final Calendar c = Calendar.getInstance();
-            int mYear = c.get(Calendar.YEAR);
-            final int mMonth = c.get(Calendar.MONTH);
-            final int mDay = c.get(Calendar.DAY_OF_MONTH);
+				Log.e("onFailure", t.toString());
+				enableProcessButton();
+			}
+		});
 
-            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
-                    new DatePickerDialog.OnDateSetListener() {
+	}
 
-                        @Override
-                        public void onDateSet(DatePicker view, int year,
-                                              int monthOfYear, int dayOfMonth) {
+	@Override
+	public void onClick(View v) {
 
-                            milestoneDueDate.setText(getString(R.string.setDueDate, year, (monthOfYear + 1), dayOfMonth));
+		if(v == milestoneDueDate) {
 
-                        }
-                    }, mYear, mMonth, mDay);
-            datePickerDialog.show();
-        }
+			final Calendar c = Calendar.getInstance();
+			int mYear = c.get(Calendar.YEAR);
+			final int mMonth = c.get(Calendar.MONTH);
+			final int mDay = c.get(Calendar.DAY_OF_MONTH);
 
-    }
+			DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
 
-    private void initCloseListener() {
-        onClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        };
-    }
+				@Override
+				public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
 
-    private void disableProcessButton() {
+					milestoneDueDate.setText(getString(R.string.setDueDate, year, (monthOfYear + 1), dayOfMonth));
 
-        createNewMilestoneButton.setEnabled(false);
-        GradientDrawable shape =  new GradientDrawable();
-        shape.setCornerRadius( 8 );
-        shape.setColor(getResources().getColor(R.color.hintColor));
-        createNewMilestoneButton.setBackground(shape);
+				}
+			}, mYear, mMonth, mDay);
+			datePickerDialog.show();
+		}
 
-    }
+	}
 
-    private void enableProcessButton() {
+	private void initCloseListener() {
 
-        createNewMilestoneButton.setEnabled(true);
-        GradientDrawable shape =  new GradientDrawable();
-        shape.setCornerRadius( 8 );
-        shape.setColor(getResources().getColor(R.color.btnBackground));
-        createNewMilestoneButton.setBackground(shape);
+		onClickListener = new View.OnClickListener() {
 
-    }
+			@Override
+			public void onClick(View view) {
+
+				finish();
+			}
+		};
+	}
+
+	private void disableProcessButton() {
+
+		createNewMilestoneButton.setEnabled(false);
+		GradientDrawable shape = new GradientDrawable();
+		shape.setCornerRadius(8);
+		shape.setColor(getResources().getColor(R.color.hintColor));
+		createNewMilestoneButton.setBackground(shape);
+
+	}
+
+	private void enableProcessButton() {
+
+		createNewMilestoneButton.setEnabled(true);
+		GradientDrawable shape = new GradientDrawable();
+		shape.setCornerRadius(8);
+		shape.setColor(getResources().getColor(R.color.btnBackground));
+		createNewMilestoneButton.setBackground(shape);
+
+	}
 
 }
