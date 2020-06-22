e76e57d621ad77eccf8efbbadeddf8e44e9393cc
6543 <6543@obermui.de>
2020-06-22 16:52:54 +0200

    rm already done one (on master)

--- app/src/main/java/org/mian/gitnex/activities/FileDiffActivity.java
+++ app/src/main/java/org/mian/gitnex/activities/FileDiffActivity.java
@@ -10,17 +10,17 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
-import org.apache.commons.io.FileUtils;
 import org.mian.gitnex.R;
 import org.mian.gitnex.adapters.FilesDiffAdapter;
 import org.mian.gitnex.clients.RetrofitClient;
 import org.mian.gitnex.helpers.AlertDialogs;
+import org.mian.gitnex.helpers.ParseDiff;
 import org.mian.gitnex.helpers.Toasty;
+import org.mian.gitnex.helpers.Version;
 import org.mian.gitnex.models.FileDiffView;
 import org.mian.gitnex.util.AppUtil;
 import org.mian.gitnex.util.TinyDB;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -28,12 +28,12 @@ import retrofit2.Callback;
 
 /**
  * Author M M Arif
- **/
+ */
 
 public class FileDiffActivity extends BaseActivity {
 
 	private View.OnClickListener onClickListener;
-	private TextView toolbar_title;
+	private TextView toolbarTitle;
 	private ListView mListView;
 	private ProgressBar mProgressBar;
 	final Context ctx = this;
@@ -59,18 +59,17 @@ public class FileDiffActivity extends BaseActivity {
 		String[] parts = repoFullName.split("/");
 		final String repoOwner = parts[0];
 		final String repoName = parts[1];
-		final String instanceUrl = tinyDb.getString("instanceUrl");
 		final String loginUid = tinyDb.getString("loginUid");
 		final String instanceToken = "token " + tinyDb.getString(loginUid + "-token");
 
 		ImageView closeActivity = findViewById(R.id.close);
-		toolbar_title = findViewById(R.id.toolbar_title);
+		toolbarTitle = findViewById(R.id.toolbar_title);
 		mListView = findViewById(R.id.listView);
 		mProgressBar = findViewById(R.id.progress_bar);
 
 		mListView.setDivider(null);
 
-		toolbar_title.setText(R.string.processingText);
+		toolbarTitle.setText(R.string.processingText);
 		initCloseListener();
 		closeActivity.setOnClickListener(onClickListener);
 
@@ -78,13 +77,28 @@ public class FileDiffActivity extends BaseActivity {
 
 		String pullIndex = tinyDb.getString("issueNumber");
 
-		getPullDiffContent(tinyDb.getString("instanceUrlWithProtocol"), repoOwner, repoName, pullIndex);
+		boolean apiCall = true;
+		String instanceUrl = tinyDb.getString("instanceUrl");
+
+		// fallback for old gitea instances
+		if(new Version(tinyDb.getString("giteaVersion")).less("1.13.0")) {
+			apiCall = false;
+			instanceUrl = tinyDb.getString("instanceUrlWithProtocol");
+		}
+
+		getPullDiffContent(instanceUrl, repoOwner, repoName, pullIndex, instanceToken, apiCall);
 
 	}
 
-	private void getPullDiffContent(String instanceUrl, String owner, String repo, String filename) {
+	private void getPullDiffContent(String instanceUrl, String owner, String repo, String pullIndex, String token, boolean apiCall) {
 
-		Call<ResponseBody> call = RetrofitClient.getInstance(instanceUrl, ctx).getWebInterface().getPullDiffContent(owner, repo, filename);
+		Call<ResponseBody> call;
+		if(apiCall) {
+			call = RetrofitClient.getInstance(instanceUrl, ctx).getApiInterface().getPullDiffContent(token, owner, repo, pullIndex);
+		}
+		else {
+			call = RetrofitClient.getInstance(instanceUrl, ctx).getWebInterface().getPullDiffContent(owner, repo, pullIndex);
+		}
 
 		call.enqueue(new Callback<ResponseBody>() {
 
@@ -97,69 +111,14 @@ public class FileDiffActivity extends BaseActivity {
 						assert response.body() != null;
 
 						AppUtil appUtil = new AppUtil();
-						List<FileDiffView> fileContentsArray = new ArrayList<>();
-
-						String[] lines = response.body().string().split("diff");
-
-						if(lines.length > 0) {
-
-							for(int i = 1; i < lines.length; i++) {
-
-								if(lines[i].contains("@@ -")) {
-
-									String[] level2nd = lines[i].split("@@ -"); // main content part of single diff view
-
-									String[] fileName_ = level2nd[0].split("\\+\\+\\+ b/"); // filename part
-									String fileNameFinal = fileName_[1];
-
-									String[] fileContents_ = level2nd[1].split("@@"); // file info / content part
-									String fileInfoFinal = fileContents_[0];
-									StringBuilder fileContentsFinal = new StringBuilder(fileContents_[1]);
-
-									if(level2nd.length > 2) {
-										for(int j = 2; j < level2nd.length; j++) {
-											fileContentsFinal.append(level2nd[j]);
-										}
-									}
-
-									String fileExtension = FileUtils.getExtension(fileNameFinal);
-
-									String fileContentsFinalWithBlankLines = fileContentsFinal.toString().replaceAll(".*@@.*", "");
-									String fileContentsFinalWithoutBlankLines = fileContentsFinal.toString().replaceAll(".*@@.*(\r?\n|\r)?", "");
-									fileContentsFinalWithoutBlankLines = fileContentsFinalWithoutBlankLines.replaceAll(".*\\ No newline at end of file.*(\r?\n|\r)?", "");
-
-									fileContentsArray.add(new FileDiffView(fileNameFinal, appUtil.imageExtension(fileExtension), fileInfoFinal, fileContentsFinalWithoutBlankLines));
-								}
-								else {
-
-									String[] getFileName = lines[i].split("--git a/");
-
-									String[] getFileName_ = getFileName[1].split("b/");
-									String getFileNameFinal = getFileName_[0].trim();
-
-									String[] binaryFile = getFileName_[1].split("GIT binary patch");
-									String binaryFileRaw = binaryFile[1].substring(binaryFile[1].indexOf('\n') + 1);
-									String binaryFileFinal = binaryFile[1].substring(binaryFileRaw.indexOf('\n') + 1);
-
-									String fileExtension = FileUtils.getExtension(getFileNameFinal);
-
-									if(appUtil.imageExtension(FileUtils.getExtension(getFileNameFinal))) {
-
-										fileContentsArray.add(new FileDiffView(getFileNameFinal, appUtil.imageExtension(fileExtension), "", binaryFileFinal));
-									}
-
-								}
-
-							}
-
-						}
+						List<FileDiffView> fileContentsArray = ParseDiff.getFileDiffViewArray(response.body().string());
 
 						int filesCount = fileContentsArray.size();
 						if(filesCount > 1) {
-							toolbar_title.setText(getResources().getString(R.string.fileDiffViewHeader, Integer.toString(filesCount)));
+							toolbarTitle.setText(getResources().getString(R.string.fileDiffViewHeader, Integer.toString(filesCount)));
 						}
 						else {
-							toolbar_title.setText(getResources().getString(R.string.fileDiffViewHeaderSingle, Integer.toString(filesCount)));
+							toolbarTitle.setText(getResources().getString(R.string.fileDiffViewHeaderSingle, Integer.toString(filesCount)));
 						}
 
 						FilesDiffAdapter adapter = new FilesDiffAdapter(ctx, fileContentsArray);
