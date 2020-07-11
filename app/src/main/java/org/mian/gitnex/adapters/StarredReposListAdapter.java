38b3db9479cbb43ad35ad48866fc0cc6fb8718f3
M M Arif <mmarif@swatian.com>
2020-07-11 14:52:27 +0500

    Switch of icons to new icons for the whole app - WIP

--- app/src/main/java/org/mian/gitnex/adapters/StarredReposListAdapter.java
+++ app/src/main/java/org/mian/gitnex/adapters/StarredReposListAdapter.java
@@ -268,7 +268,7 @@ public class StarredReposListAdapter extends RecyclerView.Adapter<StarredReposLi
             holder.repoType.setText(R.string.strPrivate);
         }
         else {
-            holder.repoPrivatePublic.setImageResource(R.drawable.ic_public);
+            holder.repoPrivatePublic.setImageResource(R.drawable.ic_unlock);
             holder.repoType.setText(R.string.strPublic);
         }
         holder.repoStars.setText(currentItem.getStars_count());
