e4e06ba1e1da1c0496569bc942b8f4a93a841db9
M M Arif <mmarif@swatian.com>
2020-09-05 23:34:16 +0500

    Pull repo info into dialog

--- app/src/main/java/org/mian/gitnex/activities/RepositorySettingsActivity.java
+++ app/src/main/java/org/mian/gitnex/activities/RepositorySettingsActivity.java
@@ -8,10 +8,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import androidx.annotation.NonNull;
 import org.mian.gitnex.R;
+import org.mian.gitnex.clients.RetrofitClient;
 import org.mian.gitnex.databinding.ActivityRepositorySettingsBinding;
 import org.mian.gitnex.databinding.CustomRepositoryEditPropertiesDialogBinding;
 import org.mian.gitnex.helpers.TinyDB;
+import org.mian.gitnex.helpers.Toasty;
+import org.mian.gitnex.models.UserRepositories;
+import retrofit2.Call;
+import retrofit2.Callback;
 
 /**
  * Author M M Arif
@@ -83,6 +89,59 @@ public class RepositorySettingsActivity extends BaseActivity {
 
 		propBinding.cancel.setOnClickListener(editProperties -> {
 			dialog.dismiss();
+		});
+
+		Call<UserRepositories> call = RetrofitClient
+			.getInstance(instanceUrl, ctx)
+			.getApiInterface()
+			.getUserRepository(instanceToken, repositoryOwner, repositoryName);
+
+		call.enqueue(new Callback<UserRepositories>() {
+
+			@Override
+			public void onResponse(@NonNull Call<UserRepositories> call, @NonNull retrofit2.Response<UserRepositories> response) {
+
+				UserRepositories repoInfo = response.body();
+
+				if (response.code() == 200) {
+
+					assert repoInfo != null;
+					propBinding.repoName.setText(repoInfo.getName());
+					propBinding.repoWebsite.setText(repoInfo.getWebsite());
+					propBinding.repoDescription.setText(repoInfo.getDescription());
+					propBinding.repoPrivate.setChecked(repoInfo.getPrivateFlag());
+					propBinding.repoAsTemplate.setChecked(repoInfo.isTemplate());
+
+					propBinding.repoEnableIssues.setChecked(repoInfo.getHas_issues());
+					if(repoInfo.getHas_issues()) {
+						propBinding.repoExternalIssueTrackerLayout.setVisibility(View.GONE);
+					}
+
+					propBinding.repoEnableWiki.setChecked(repoInfo.isHas_wiki());
+					if(repoInfo.isHas_wiki()) {
+						propBinding.repoExternalWikiLayout.setVisibility(View.GONE);
+					}
+
+					propBinding.repoEnablePr.setChecked(repoInfo.isHas_pull_requests());
+					propBinding.repoEnableTimer.setChecked(repoInfo.getInternal_tracker().isEnable_time_tracker());
+					propBinding.repoEnableMerge.setChecked(repoInfo.isAllow_merge_commits());
+					propBinding.repoEnableRebase.setChecked(repoInfo.isAllow_rebase());
+					propBinding.repoEnableSquash.setChecked(repoInfo.isAllow_squash_merge());
+					propBinding.repoEnableForceMerge.setChecked(repoInfo.isAllow_rebase_explicit());
+
+				}
+				else {
+
+					Toasty.error(ctx, getString(R.string.genericError));
+				}
+
+			}
+
+			@Override
+			public void onFailure(@NonNull Call<UserRepositories> call, @NonNull Throwable t) {
+
+				Toasty.error(ctx, getString(R.string.genericServerResponseError));
+			}
 		});
 
 		dialog.show();
