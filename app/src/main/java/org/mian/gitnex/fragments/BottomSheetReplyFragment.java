1c0d3c3cf2b060558d5e52ff236a9e92a2124c86
M M Arif <mmarif@swatian.com>
2020-10-12 18:31:02 +0500

    Fix statusbar color, remove social, enhance other things

--- app/src/main/java/org/mian/gitnex/fragments/BottomSheetReplyFragment.java
+++ app/src/main/java/org/mian/gitnex/fragments/BottomSheetReplyFragment.java
@@ -9,25 +9,20 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
-import com.hendraanggrian.appcompat.socialview.Mention;
-import com.hendraanggrian.appcompat.widget.MentionArrayAdapter;
-import com.hendraanggrian.appcompat.widget.SocialAutoCompleteTextView;
 import org.mian.gitnex.R;
 import org.mian.gitnex.actions.ActionResult;
-import org.mian.gitnex.actions.CollaboratorActions;
 import org.mian.gitnex.actions.IssueActions;
 import org.mian.gitnex.activities.MainActivity;
 import org.mian.gitnex.database.api.DraftsApi;
 import org.mian.gitnex.helpers.StaticGlobalVariables;
 import org.mian.gitnex.helpers.TinyDB;
 import org.mian.gitnex.helpers.Toasty;
-import org.mian.gitnex.models.Collaborators;
 import java.util.Objects;
 
 /**
@@ -44,7 +39,6 @@ public class BottomSheetReplyFragment extends BottomSheetDialogFragment {
 	private int issueNumber;
 	private long draftId;
 
-	private ArrayAdapter<Mention> mentionArrayAdapter;
 	private TextView draftsHint;
 
 	@Override
@@ -57,22 +51,7 @@ public class BottomSheetReplyFragment extends BottomSheetDialogFragment {
 		currentActiveAccountId = tinyDB.getInt("currentActiveAccountId");
 		issueNumber = Integer.parseInt(tinyDB.getString("issueNumber"));
 
-		mentionArrayAdapter = new MentionArrayAdapter<>(context);
-
-		CollaboratorActions.getCollaborators(context).accept((status, result) -> {
-
-			assert result != null;
-
-			for(Collaborators collaborators : result) {
-
-				Mention mention = new Mention(collaborators.getUsername(), collaborators.getFull_name(), collaborators.getAvatar_url());
-				mentionArrayAdapter.add(mention);
-
-			}
-		});
-
 		super.onAttach(context);
-
 	}
 
 	@Nullable
@@ -84,23 +63,30 @@ public class BottomSheetReplyFragment extends BottomSheetDialogFragment {
 
 		draftsHint = view.findViewById(R.id.drafts_hint);
 
-		SocialAutoCompleteTextView socialAutoCompleteTextView = view.findViewById(R.id.comment);
+		EditText commentContent = view.findViewById(R.id.comment);
 		TextView toolbarTitle = view.findViewById(R.id.toolbar_title);
 		ImageButton close = view.findViewById(R.id.close);
 		ImageButton drafts = view.findViewById(R.id.drafts);
 		ImageButton send = view.findViewById(R.id.send);
 
+		send.setEnabled(false);
+
 		if(Objects.equals(arguments.getString("commentAction"), "edit")) {
+
 			send.setVisibility(View.GONE);
 		}
 
 		if(arguments.getString("draftId") != null) {
+
 			draftId = Long.parseLong(arguments.getString("draftId"));
 		}
 
 		if(!tinyDB.getString("issueTitle").isEmpty()) {
+
 			toolbarTitle.setText(tinyDB.getString("issueTitle"));
-		} else if(arguments.getString("draftTitle") != null) {
+		}
+		else if(arguments.getString("draftTitle") != null) {
+
 			toolbarTitle.setText(arguments.getString("draftTitle"));
 		}
 
@@ -109,39 +95,38 @@ public class BottomSheetReplyFragment extends BottomSheetDialogFragment {
 			send.setEnabled(true);
 			send.setAlpha(1f);
 
-			socialAutoCompleteTextView.setText(arguments.getString("commentBody"));
+			commentContent.setText(arguments.getString("commentBody"));
 
 			if(arguments.getBoolean("cursorToEnd", false)) {
-				socialAutoCompleteTextView.setSelection(socialAutoCompleteTextView.length());
+
+				commentContent.setSelection(commentContent.length());
 			}
 		}
 
-		socialAutoCompleteTextView.requestFocus();
-		socialAutoCompleteTextView.addTextChangedListener(new TextWatcher() {
+		commentContent.requestFocus();
+		commentContent.addTextChangedListener(new TextWatcher() {
 
 			@Override
 			public void afterTextChanged(Editable s) {
 
-				String text = socialAutoCompleteTextView.getText().toString();
+				String text = commentContent.getText().toString();
 
 				if(text.isEmpty()) {
 
 					send.setEnabled(false);
 					send.setAlpha(0.5f);
 					saveDraft(null, true);
-
-				} else {
+				}
+				else {
 
 					send.setEnabled(true);
 					send.setAlpha(1f);
 					saveDraft(text, false);
-
 				}
 			}
 
 			@Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 			@Override public void onTextChanged(CharSequence s, int start, int before, int count) {}
-
 		});
 
 		close.setOnClickListener(v -> dismiss());
@@ -153,11 +138,10 @@ public class BottomSheetReplyFragment extends BottomSheetDialogFragment {
 			startActivity(intent);
 
 			dismiss();
-
 		});
 
 		send.setOnClickListener(v -> IssueActions
-			.reply(getContext(), socialAutoCompleteTextView.getText().toString(), issueNumber)
+			.reply(getContext(), commentContent.getText().toString(), issueNumber)
 			.accept((status, result) -> {
 
 				if(status == ActionResult.Status.SUCCESS) {
@@ -169,21 +153,20 @@ public class BottomSheetReplyFragment extends BottomSheetDialogFragment {
 					tinyDB.putBoolean("resumePullRequests", true);
 
 					if(draftId != 0 && tinyDB.getBoolean("draftsCommentsDeletionEnabled")) {
+
 						draftsApi.deleteSingleDraft((int) draftId);
 					}
 
 					dismiss();
-
-				} else {
+				}
+				else {
 
 					Toasty.error(getContext(), getString(R.string.commentError));
 					dismiss();
-
 				}
 			}));
 
 		return view;
-
 	}
 
 	private void saveDraft(String text, boolean remove) {
@@ -199,7 +182,6 @@ public class BottomSheetReplyFragment extends BottomSheetDialogFragment {
 			}
 
 			draftsHint.setAlpha(value);
-
 		});
 
 		if(remove) {
@@ -208,8 +190,8 @@ public class BottomSheetReplyFragment extends BottomSheetDialogFragment {
 			draftId = 0;
 
 			valueAnimator.reverse();
-
-		} else {
+		}
+		else {
 
 			if(draftId == 0) {
 				draftId = draftsApi.insertDraft(repositoryId, currentActiveAccountId, issueNumber, text, StaticGlobalVariables.draftTypeComment, "TODO");
@@ -219,7 +201,6 @@ public class BottomSheetReplyFragment extends BottomSheetDialogFragment {
 
 			draftsHint.setVisibility(View.VISIBLE);
 			valueAnimator.start();
-
 		}
 	}
 
@@ -229,6 +210,5 @@ public class BottomSheetReplyFragment extends BottomSheetDialogFragment {
 		fragment.setArguments(bundle);
 
 		return fragment;
-
 	}
 }
