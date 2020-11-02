04e8ac6d8fcec39619a37652ba71b104b6cc0978
opyale <opyale@noreply.codeberg.org>
2020-11-02 13:36:29 +0100

    Calculating density for avatars.

--- app/src/main/java/org/mian/gitnex/adapters/IssueCommentsAdapter.java
+++ app/src/main/java/org/mian/gitnex/adapters/IssueCommentsAdapter.java
@@ -12,7 +12,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
@@ -23,6 +22,7 @@ import org.mian.gitnex.clients.PicassoService;
 import org.mian.gitnex.clients.RetrofitClient;
 import org.mian.gitnex.fragments.BottomSheetReplyFragment;
 import org.mian.gitnex.helpers.AlertDialogs;
+import org.mian.gitnex.helpers.AppUtil;
 import org.mian.gitnex.helpers.Markdown;
 import org.mian.gitnex.helpers.RoundedTransformation;
 import org.mian.gitnex.helpers.TimeHelper;
@@ -291,7 +291,7 @@ public class IssueCommentsAdapter extends RecyclerView.Adapter<IssueCommentsAdap
 			.load(issueComment.getUser().getAvatar_url())
 			.placeholder(R.drawable.loader_animated)
 			.transform(new RoundedTransformation(4, 0))
-			.resize(40, 40)
+			.resize(AppUtil.getPixelsFromDensity(ctx, 35), AppUtil.getPixelsFromDensity(ctx, 35))
 			.centerCrop()
 			.into(holder.avatar);
 
