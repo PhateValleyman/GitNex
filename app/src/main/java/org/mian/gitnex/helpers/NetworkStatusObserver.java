aff8ca861e34dd0cba2ae969be07e1ecf9036f2f
opyale <opyale@noreply.codeberg.org>
2021-01-02 16:04:03 +0100

    Some fixes.

--- app/src/main/java/org/mian/gitnex/helpers/NetworkStatusObserver.java
+++ app/src/main/java/org/mian/gitnex/helpers/NetworkStatusObserver.java
@@ -3,10 +3,10 @@ package org.mian.gitnex.helpers;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.Network;
+import android.net.NetworkCapabilities;
 import android.net.NetworkRequest;
 import androidx.annotation.NonNull;
 import java.util.concurrent.atomic.AtomicBoolean;
-import static android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET;
 
 /**
  * @author opyale
@@ -15,7 +15,7 @@ public class NetworkStatusObserver {
 
 	private static NetworkStatusObserver networkStatusObserver;
 
-	private final AtomicBoolean hasNetworkConnection = new AtomicBoolean();
+	private final AtomicBoolean hasNetworkConnection = new AtomicBoolean(false);
 	private boolean hasInitialized = false;
 
 	private final Object mutex = new Object();
@@ -25,7 +25,8 @@ public class NetworkStatusObserver {
 		ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
 		NetworkRequest networkRequest = new NetworkRequest.Builder()
-			.addCapability(NET_CAPABILITY_INTERNET)
+			.addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+			.addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_RESTRICTED)
 			.build();
 
 		cm.requestNetwork(networkRequest, new ConnectivityManager.NetworkCallback() {
@@ -37,7 +38,7 @@ public class NetworkStatusObserver {
 			}
 
 			@Override
-			public void onUnavailable() {
+			public void onLost(@NonNull Network network) {
 				hasNetworkConnection.set(false);
 				checkInitialized();
 			}
@@ -45,8 +46,8 @@ public class NetworkStatusObserver {
 			private void checkInitialized() {
 
 				if(!hasInitialized) {
+					hasInitialized = true;
 					synchronized(mutex) {
-						hasInitialized = true;
 						mutex.notify();
 					}
 				}
@@ -56,7 +57,11 @@ public class NetworkStatusObserver {
 
 		try {
 			synchronized(mutex) {
-				mutex.wait();
+				// This is actually not the recommended way to do this, but there
+				// is no other option besides upgrading to API level 26
+				// in order to use the built-in timeout functionality of {@code requestNetwork()}
+				// which in turn gives us access to {@code onUnavailable()} .
+				mutex.wait(5);
 			}
 		} catch(InterruptedException ignored) {}
 	}
