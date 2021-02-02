5578ff397facebd9795ea7d4a8482166e228a4cf
M M Arif <mmarif@swatian.com>
2021-02-02 22:41:09 +0500

    Add info popup dialog for details

--- app/src/main/java/org/mian/gitnex/adapters/AdminCronTasksAdapter.java
+++ app/src/main/java/org/mian/gitnex/adapters/AdminCronTasksAdapter.java
@@ -5,12 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
+import org.apache.commons.lang3.StringUtils;
 import org.mian.gitnex.R;
+import org.mian.gitnex.helpers.TimeHelper;
+import org.mian.gitnex.helpers.TinyDB;
 import org.mian.gitnex.models.CronTasks;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Author M M Arif
@@ -23,22 +29,59 @@ public class AdminCronTasksAdapter extends RecyclerView.Adapter<AdminCronTasksAd
 
 	static class CronTasksViewHolder extends RecyclerView.ViewHolder {
 
+		private CronTasks cronTasks;
+
 		private final ImageView runTask;
 		private final TextView taskName;
-		/*private final TextView schedule;
-		private final TextView next;
-		private final TextView prev;
-		private final int execTimes;*/
+		private final LinearLayout cronTasksInfo;
+		private final LinearLayout cronTasksRun;
 
 		private CronTasksViewHolder(View itemView) {
+
 			super(itemView);
+			Context ctx = itemView.getContext();
+			TinyDB tinyDb = TinyDB.getInstance(ctx);
+
+			final String locale = tinyDb.getString("locale");
+			final String timeFormat = tinyDb.getString("dateFormat");
 
 			runTask = itemView.findViewById(R.id.runTask);
 			taskName = itemView.findViewById(R.id.taskName);
-			/*schedule = itemView.findViewById(R.id.schedule);
-			next = itemView.findViewById(R.id.next);
-			prev = itemView.findViewById(R.id.prev);
-			execTimes = itemView.findViewById(R.id.execTimes);*/
+			cronTasksInfo = itemView.findViewById(R.id.cronTasksInfo);
+			cronTasksRun = itemView.findViewById(R.id.cronTasksRun);
+
+			cronTasksInfo.setOnClickListener(taskInfo -> {
+
+				String nextRun = "";
+				String lastRun = "";
+
+				if(cronTasks.getNext() != null) {
+					nextRun = TimeHelper.formatTime(cronTasks.getNext(), new Locale(locale), timeFormat, ctx);
+				}
+				if(cronTasks.getPrev() != null) {
+					lastRun = TimeHelper.formatTime(cronTasks.getPrev(), new Locale(locale), timeFormat, ctx);
+				}
+
+				View view = LayoutInflater.from(ctx).inflate(R.layout.layout_cron_task_info, null);
+
+				TextView taskScheduleContent = view.findViewById(R.id.taskScheduleContent);
+				TextView nextRunContent = view.findViewById(R.id.nextRunContent);
+				TextView lastRunContent = view.findViewById(R.id.lastRunContent);
+				TextView execTimeContent = view.findViewById(R.id.execTimeContent);
+
+				taskScheduleContent.setText(cronTasks.getSchedule());
+				nextRunContent.setText(nextRun);
+				lastRunContent.setText(lastRun);
+				execTimeContent.setText(String.valueOf(cronTasks.getExec_times()));
+
+				AlertDialog.Builder alertDialog = new AlertDialog.Builder(ctx);
+
+				alertDialog.setTitle(StringUtils.capitalize(cronTasks.getName().replace("_", " ")));
+				alertDialog.setView(view);
+				alertDialog.setPositiveButton(ctx.getString(R.string.close), null);
+				alertDialog.create().show();
+
+			});
 		}
 	}
 
@@ -61,7 +104,8 @@ public class AdminCronTasksAdapter extends RecyclerView.Adapter<AdminCronTasksAd
 
 		CronTasks currentItem = tasksList.get(position);
 
-		holder.taskName.setText(currentItem.getName());
+		holder.cronTasks = currentItem;
+		holder.taskName.setText(StringUtils.capitalize(currentItem.getName().replace("_", " ")));
 	}
 
 	@Override
