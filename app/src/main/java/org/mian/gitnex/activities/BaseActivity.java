aebe3c442e6f024c7bbe3af7c6004500d75f68cf
M M Arif <mmarif@swatian.com>
2021-03-30 19:09:33 +0500

    Move acra to application

--- app/src/main/java/org/mian/gitnex/activities/BaseActivity.java
+++ app/src/main/java/org/mian/gitnex/activities/BaseActivity.java
@@ -3,15 +3,6 @@ package org.mian.gitnex.activities;
 import android.content.Context;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import org.acra.ACRA;
-import org.acra.BuildConfig;
-import org.acra.ReportField;
-import org.acra.annotation.AcraCore;
-import org.acra.annotation.AcraNotification;
-import org.acra.config.CoreConfigurationBuilder;
-import org.acra.config.LimiterConfigurationBuilder;
-import org.acra.config.MailSenderConfigurationBuilder;
-import org.acra.data.StringFormat;
 import org.mian.gitnex.R;
 import org.mian.gitnex.helpers.AppUtil;
 import org.mian.gitnex.helpers.TimeHelper;
@@ -21,14 +12,6 @@ import org.mian.gitnex.notifications.Notifications;
 /**
  * Author M M Arif
  */
-
-@AcraNotification(resIcon = R.drawable.gitnex_transparent,
-	resTitle = R.string.crashTitle,
-	resChannelName = R.string.setCrashReports,
-	resText = R.string.crashMessage)
-@AcraCore(reportContent = {
-	ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL,
-	ReportField.STACK_TRACE, ReportField.AVAILABLE_MEM_SIZE, ReportField.BRAND })
 
 public abstract class BaseActivity extends AppCompatActivity {
 
@@ -96,18 +79,6 @@ public abstract class BaseActivity extends AppCompatActivity {
 
 		AppUtil.setAppLocale(getResources(), tinyDB.getString("locale"));
 
-		if(tinyDB.getBoolean("crashReportingEnabled")) {
-
-			CoreConfigurationBuilder ACRABuilder = new CoreConfigurationBuilder(this);
-
-			ACRABuilder.setBuildConfigClass(BuildConfig.class).setReportFormat(StringFormat.KEY_VALUE_LIST);
-			ACRABuilder.getPluginConfigurationBuilder(MailSenderConfigurationBuilder.class).setReportAsFile(true).setMailTo(getResources().getString(R.string.appEmail)).setSubject(getResources().getString(R.string.crashReportEmailSubject, AppUtil.getAppBuildNo(getApplicationContext()))).setEnabled(true);
-			ACRABuilder.getPluginConfigurationBuilder(LimiterConfigurationBuilder.class).setEnabled(true);
-
-			ACRA.init(getApplication(), ACRABuilder);
-		}
-
-		Notifications.createChannels(appCtx);
 		Notifications.startWorker(appCtx);
 	}
 
--- app/src/main/java/org/mian/gitnex/core/MainApplication.java
+++ app/src/main/java/org/mian/gitnex/core/MainApplication.java
@@ -3,16 +3,35 @@ package org.mian.gitnex.core;
 import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.Context;
+import org.acra.ACRA;
+import org.acra.BuildConfig;
+import org.acra.ReportField;
+import org.acra.annotation.AcraCore;
+import org.acra.annotation.AcraNotification;
+import org.acra.config.CoreConfigurationBuilder;
+import org.acra.config.LimiterConfigurationBuilder;
+import org.acra.config.MailSenderConfigurationBuilder;
+import org.acra.data.StringFormat;
 import org.mian.gitnex.R;
+import org.mian.gitnex.helpers.AppUtil;
 import org.mian.gitnex.helpers.Constants;
 import org.mian.gitnex.helpers.FontsOverride;
 import org.mian.gitnex.helpers.TinyDB;
+import org.mian.gitnex.notifications.Notifications;
 
 /**
  * @author opyale
  */
 
 @SuppressLint("NonConstantResourceId")
+@AcraNotification(resIcon = R.drawable.gitnex_transparent,
+	resTitle = R.string.crashTitle,
+	resChannelName = R.string.setCrashReports,
+	resText = R.string.crashMessage)
+@AcraCore(reportContent = {
+	ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL,
+	ReportField.STACK_TRACE, ReportField.AVAILABLE_MEM_SIZE, ReportField.BRAND })
+
 public class MainApplication extends Application {
 
 	private Context appCtx;
@@ -52,6 +71,26 @@ public class MainApplication extends Application {
 
 		}
 
+		Notifications.createChannels(appCtx);
+	}
+
+	@Override
+	protected void attachBaseContext(Context context) {
+		super.attachBaseContext(context);
+
+		tinyDB = TinyDB.getInstance(context);
+
+		if(tinyDB.getBoolean("crashReportingEnabled")) {
+
+			CoreConfigurationBuilder ACRABuilder = new CoreConfigurationBuilder(this);
+
+			ACRABuilder.setBuildConfigClass(BuildConfig.class).setReportFormat(StringFormat.KEY_VALUE_LIST);
+			ACRABuilder.getPluginConfigurationBuilder(MailSenderConfigurationBuilder.class).setReportAsFile(true).setMailTo(getResources().getString(R.string.appEmail)).setSubject(getResources().getString(R.string.crashReportEmailSubject, AppUtil
+				.getAppBuildNo(context))).setEnabled(true);
+			ACRABuilder.getPluginConfigurationBuilder(LimiterConfigurationBuilder.class).setEnabled(true);
+
+			ACRA.init(this, ACRABuilder);
+		}
 	}
 
 	private void setDefaults() {
