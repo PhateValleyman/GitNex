266b26d9cf4aebe4a8439784975c90ef3e4179fe
M M Arif <mmarif@swatian.com>
2021-03-30 18:23:57 +0500

    Fix ACRA and workmanager errors

--- app/src/main/java/org/mian/gitnex/core/MainApplication.java
+++ app/src/main/java/org/mian/gitnex/core/MainApplication.java
@@ -3,35 +3,16 @@ package org.mian.gitnex.core;
 import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.Context;
-import org.acra.ACRA;
-import org.acra.BuildConfig;
-import org.acra.ReportField;
-import org.acra.annotation.AcraCore;
-import org.acra.annotation.AcraNotification;
-import org.acra.config.CoreConfigurationBuilder;
-import org.acra.config.LimiterConfigurationBuilder;
-import org.acra.config.MailSenderConfigurationBuilder;
-import org.acra.data.StringFormat;
 import org.mian.gitnex.R;
-import org.mian.gitnex.helpers.AppUtil;
 import org.mian.gitnex.helpers.Constants;
 import org.mian.gitnex.helpers.FontsOverride;
 import org.mian.gitnex.helpers.TinyDB;
-import org.mian.gitnex.notifications.Notifications;
 
 /**
  * @author opyale
  */
 
 @SuppressLint("NonConstantResourceId")
-@AcraNotification(resIcon = R.drawable.gitnex_transparent,
-	resTitle = R.string.crashTitle,
-	resChannelName = R.string.setCrashReports,
-	resText = R.string.crashMessage)
-@AcraCore(reportContent = {
-	ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL,
-	ReportField.STACK_TRACE, ReportField.AVAILABLE_MEM_SIZE, ReportField.BRAND })
-
 public class MainApplication extends Application {
 
 	private Context appCtx;
@@ -70,21 +51,6 @@ public class MainApplication extends Application {
 				FontsOverride.setDefaultFont(this, "SANS_SERIF", "fonts/manroperegular.ttf");
 
 		}
-
-		if(tinyDB.getBoolean("crashReportingEnabled")) {
-
-			CoreConfigurationBuilder ACRABuilder = new CoreConfigurationBuilder(this);
-
-			ACRABuilder.setBuildConfigClass(BuildConfig.class).setReportFormat(StringFormat.KEY_VALUE_LIST);
-			ACRABuilder.getPluginConfigurationBuilder(MailSenderConfigurationBuilder.class).setReportAsFile(true).setMailTo(getResources().getString(R.string.appEmail)).setSubject(getResources().getString(R.string.crashReportEmailSubject, AppUtil.getAppBuildNo(getApplicationContext()))).setEnabled(true);
-			ACRABuilder.getPluginConfigurationBuilder(LimiterConfigurationBuilder.class).setEnabled(true);
-
-			ACRA.init(this, ACRABuilder);
-
-		}
-
-		Notifications.createChannels(appCtx);
-		Notifications.startWorker(appCtx);
 
 	}
 
