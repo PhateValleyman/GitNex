28110fb85f25f4306b0c5e50876ff169f96886fe
qwerty287 <ndev@web.de>
2021-05-28 08:38:27 +0200

    Clean up imports

--- app/src/main/java/org/mian/gitnex/activities/SettingsTranslationActivity.java
+++ app/src/main/java/org/mian/gitnex/activities/SettingsTranslationActivity.java
@@ -2,7 +2,6 @@ package org.mian.gitnex.activities;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.AssetManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
