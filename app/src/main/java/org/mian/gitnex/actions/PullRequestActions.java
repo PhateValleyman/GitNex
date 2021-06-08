36cc884f77d563ac7029c7f1d4e0996529654e50
qwerty287 <ndev@web.de>
2021-06-08 16:28:41 +0200

    Correct tabs

--- app/src/main/java/org/mian/gitnex/actions/PullRequestActions.java
+++ app/src/main/java/org/mian/gitnex/actions/PullRequestActions.java
@@ -23,41 +23,41 @@ public class PullRequestActions {
 				.getApiInterface(context)
 				.deleteBranch(Authorization.get(context), repoOwner, repoName, headBranch);
 
-			call.enqueue(new Callback<JsonElement>() {
+		call.enqueue(new Callback<JsonElement>() {
 
-				@Override
-				public void onResponse(@NonNull Call<JsonElement> call, @NonNull retrofit2.Response<JsonElement> response) {
+			@Override
+			public void onResponse(@NonNull Call<JsonElement> call, @NonNull retrofit2.Response<JsonElement> response) {
 
-					if(response.code() == 204) {
+				if(response.code() == 204) {
 
-						if(showToasts) Toasty.success(context, context.getString(R.string.deleteBranchSuccess));
-					}
-					else if(response.code() == 401) {
-
-						AlertDialogs
-							.authorizationTokenRevokedDialog(context, context.getResources().getString(R.string.alertDialogTokenRevokedTitle), context.getResources().getString(R.string.alertDialogTokenRevokedMessage), context.getResources().getString(R.string.alertDialogTokenRevokedCopyNegativeButton), context.getResources().getString(R.string.alertDialogTokenRevokedCopyPositiveButton));
-					}
-					else if(response.code() == 403) {
-
-						if(showToasts) Toasty.error(context, context.getString(R.string.authorizeError));
-					}
-					else if(response.code() == 404) {
-
-						if(showToasts) Toasty.warning(context, context.getString(R.string.deleteBranchErrorNotFound));
-					}
-					else {
-
-						if(showToasts) Toasty.error(context, context.getString(R.string.genericError));
-					}
+					if(showToasts) Toasty.success(context, context.getString(R.string.deleteBranchSuccess));
 				}
+				else if(response.code() == 401) {
 
-				@Override
-				public void onFailure(@NonNull Call<JsonElement> call, @NonNull Throwable t) {
-
-					if(showToasts) Toasty.error(context, context.getString(R.string.deleteBranchError));
+					AlertDialogs
+						.authorizationTokenRevokedDialog(context, context.getResources().getString(R.string.alertDialogTokenRevokedTitle), context.getResources().getString(R.string.alertDialogTokenRevokedMessage), context.getResources().getString(R.string.alertDialogTokenRevokedCopyNegativeButton), context.getResources().getString(R.string.alertDialogTokenRevokedCopyPositiveButton));
 				}
+				else if(response.code() == 403) {
 
-			});
+					if(showToasts) Toasty.error(context, context.getString(R.string.authorizeError));
+				}
+				else if(response.code() == 404) {
+
+					if(showToasts) Toasty.warning(context, context.getString(R.string.deleteBranchErrorNotFound));
+				}
+				else {
+
+					if(showToasts) Toasty.error(context, context.getString(R.string.genericError));
+				}
+			}
+
+			@Override
+			public void onFailure(@NonNull Call<JsonElement> call, @NonNull Throwable t) {
+
+				if(showToasts) Toasty.error(context, context.getString(R.string.deleteBranchError));
+			}
+
+		});
 	}
 
 }
