1329fd1c8acc53eaa35bedf9ce4b756d040c296d
qwerty287 <ndev@web.de>
2021-06-08 16:28:59 +0200

    Remove import statement

--- app/src/main/java/org/mian/gitnex/actions/PullRequestActions.java
+++ app/src/main/java/org/mian/gitnex/actions/PullRequestActions.java
@@ -1,7 +1,6 @@
 package org.mian.gitnex.actions;
 
 import android.content.Context;
-import android.util.Log;
 import androidx.annotation.NonNull;
 import com.google.gson.JsonElement;
 import org.mian.gitnex.R;
