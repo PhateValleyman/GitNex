2836b4026b160d7899b18e9196e9d03892805dd2
qwerty287 <ndev@web.de>
2022-03-27 17:03:28 +0200

    Downgrade stormport

--- app/src/main/java/org/mian/gitnex/helpers/Markdown.java
+++ app/src/main/java/org/mian/gitnex/helpers/Markdown.java
@@ -61,6 +61,8 @@ import de.qwerty287.markwonprism4j.Prism4jThemeDefault;
 import de.qwerty287.markwonprism4j.SyntaxHighlightPlugin;
 import io.noties.prism4j.Prism4j;
 import stormpot.Allocator;
+import stormpot.BlazePool;
+import stormpot.Config;
 import stormpot.Pool;
 import stormpot.Poolable;
 import stormpot.Slot;
@@ -85,7 +87,12 @@ public class Markdown {
 
 	static {
 
-		rendererPool = Pool.from(new Allocator<Renderer>() {
+		Config<Renderer> config = new Config<>();
+
+		config.setBackgroundExpirationEnabled(true);
+		config.setPreciseLeakDetectionEnabled(true);
+		config.setSize(MAX_OBJECT_POOL_SIZE);
+		config.setAllocator(new Allocator<Renderer>() {
 
 			@Override
 			public Renderer allocate(Slot slot) {
@@ -94,13 +101,16 @@ public class Markdown {
 
 			@Override public void deallocate(Renderer poolable) {}
 
-		})
-			.setSize(MAX_OBJECT_POOL_SIZE)
-			.setPreciseLeakDetectionEnabled(true)
-			.setBackgroundExpirationEnabled(true)
-			.build();
+		});
 
-		rvRendererPool = Pool.from(new Allocator<RecyclerViewRenderer>() {
+		rendererPool = new BlazePool<>(config);
+
+		Config<RecyclerViewRenderer> configRv = new Config<>();
+
+		configRv.setBackgroundExpirationEnabled(true);
+		configRv.setPreciseLeakDetectionEnabled(true);
+		configRv.setSize(MAX_OBJECT_POOL_SIZE);
+		configRv.setAllocator(new Allocator<RecyclerViewRenderer>() {
 
 			@Override
 			public RecyclerViewRenderer allocate(Slot slot) {
@@ -113,11 +123,9 @@ public class Markdown {
 
 			}
 
-		})
-			.setSize(MAX_OBJECT_POOL_SIZE)
-			.setBackgroundExpirationEnabled(true)
-			.setPreciseLeakDetectionEnabled(true)
-			.build();
+		});
+
+		rvRendererPool = new BlazePool<>(configRv);
 
 	}
 
