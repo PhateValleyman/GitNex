4b30a8da5fd1a595862a3f06d444d15717187840
6543 <6543@obermui.de>
2022-08-26 18:16:59 +0200

    use own script

---
+++ format.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function getBinary() {
+    echo $(whereis $1 | cut -d ':' -f 2 | tr -d '[:space:]')
+}
+
+intelij=$(getBinary intellij-idea-ultimate-edition)
+[ -z "$intelij" ] && {
+    intelij=$(getBinary intellij-idea-community-edition)
+}
+[ -z "$intelij" ] && {
+    echo "Couldn't format files, IntelliJ binary is missing"
+    exit 1
+}
+
+$intelij format -s ".idea/codeStyles/Project.xml" -m "${PLUGIN_FILE_PATTERN:-"*"}" -r .
