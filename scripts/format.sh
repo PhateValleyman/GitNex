f62096932ceb8823a757cd945b064b55d74e7750
6543 <6543@obermui.de>
2022-09-01 18:20:34 +0200

    add more binary and make binary probe func scale

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
+probe_intelij_binarys=("intellij-idea-ultimate-edition" "intellij-idea-community-edition" "android-studio" "intellij" "intelli")
+
 function getBinary() {
     echo $(whereis $1 | cut -d ':' -f 2 | tr -d '[:space:]')
 }
 
-intelij=$(getBinary intellij-idea-ultimate-edition)
-[ -z "$intelij" ] && {
-    intelij=$(getBinary intellij-idea-community-edition)
-}
-[ -z "$intelij" ] && {
-    intelij=$(getBinary android-studio)
-}
-
-[ -z "$intelij" ] && {
-    echo "Couldn't format files, IntelliJ binary is missing"
-    exit 1
-}
+intelij=
+for bin in $probe_intelij_binarys; do
+    intelij=$(getBinary $bin)
+    [ -n "$intelij" ] && break
+done
 
 $intelij format -s ".idea/codeStyles/Project.xml" -m "${PLUGIN_FILE_PATTERN:-"*"}" -r .
